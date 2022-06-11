863f3a75f1f1efcb221cd0d96c838949269a17ef
SujinRoh <suuz.hha@gmail.com>
2022-06-11 14:26:30 +0900

    create component .vue file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vue-intermediate
 vue 를 이용해 to-do list 제작
+
++ to-do list web 
--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,13 +1,15 @@
 <template>
-  
+    <div>
+        header
+    </div>
 </template>
 
 <script>
 export default {
-
+    // js scripit
 }
 </script>
 
 <style>
-
+/* css */
 </style>
