f402d7c9d03618f244e324d6f3ea34a7dfd1994a
SujinRoh <suuz.hha@gmail.com>
2022-06-21 16:15:57 +0900

    모듈화 2

--- vue-todo/src/store/index.js
+++ vue-todo/src/store/index.js
@@ -2,30 +2,13 @@ import { createStore } from 'vuex'
 
 import todoApp from './modules/todoApp'
 
-import * as getters from './getters'
-import * as mutations from './mutatioms'
+// import * as getters from './getters'
+// import * as mutations from './mutatioms'
 
-/**
- * 로컬스토레지에 있는 todo 아이템들을 갱신받아옴
- */
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        } 
-        return arr;  
-    }
-};
+
 
 export const store = createStore({
-  state: {
-    todoItems: storage.fetch()
+  modules: {
+    todoApp
   }
-  , getters
-  , mutations
 });
